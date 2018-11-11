87c7b120bbe88be736c8fa9f956667dc1560874b
Dang Minh Tien <dangminhtien06@gmail.com>
2018-11-11 14:09:05 +0700

    Update Dagger

--- game/Dagger.cpp
+++ game/Dagger.cpp
@@ -55,40 +55,14 @@ bool Dagger::isCollision(Object * obj)
 {  
 	// dt, dx, dy đã update
 
-	float l, t, r, b;
-	float l1, t1, r1, b1;
-	this->GetBoundingBox(l, t, r, b);  // lấy BBOX của simon
-
-	obj->GetBoundingBox(l1, t1, r1, b1);
-	if (Game::GetInstance()->AABBCheck(l, t, r, b, l1, t1, r1, b1) == true)
-	{
-		return true; // check with AABB
-	}
-	 
-
-	vector<LPOBJECT> listObject;
-	listObject.clear(); 
-
-	listObject.push_back(obj);
-
-	vector<LPCOLLISIONEVENT> coEvents;
- 
-	coEvents.clear();
-
-	CalcPotentialCollisions(&listObject, coEvents); // Lấy danh sách các va chạm
-	int countCollision = (int)coEvents.size();
-
-	for (UINT i = 0; i < coEvents.size(); i++)
-		delete coEvents[i];
-
-	return (countCollision > 0);
+	return isCollitionObjectWithObject(obj);
 }
 
 void Dagger::Render(Camera * camera)
 {
 	// ra khỏi cam coi như kết thúc
 	// lẽ ra viết trong hàm update, nhưng ko có camera nên viết tạm ở đây
-	if (x - camera->GetXCam() < 0 || x - camera->GetXCam() > camera->GetWidth())
+	if (x - camera->GetXCam() + _sprite->_texture->FrameWidth < 0 || x - camera->GetXCam() > camera->GetWidth())
 	{
 		isFinish = true;
 		return; 
